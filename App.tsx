e5c4ec863d9f642821378e9a49010437b4585a36
daviddflix <daviddflix@gmail.com>
2023-11-28 19:02:05 -0300

    update home

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
 import useDimensions from './hooks/useDimensions';
 import BottomMenu from './components/BottomMenu/Menu';
 import Home from './components/Home/Home';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import TickerTape from './components/Home/Tickertape/TickerTape';
 
 const App = () => {
   const {height, width} = useDimensions();
   return (
     <GestureHandlerRootView style={{flex: 1, height, width}}>
-      {/* <ScrollView style={{backgroundColor: '#DDE1E2'}}>
-        <Home />
-      </ScrollView> */}
+      <Home />
       <BottomMenu />
     </GestureHandlerRootView>
   );
--- components/Home/HomeStyles.js
+++ components/Home/HomeStyles.js
@@ -5,12 +5,7 @@ const styles = StyleSheet.create({
   mainScrollView: {
     backgroundColor: '#DDE1E2',
     flex: 1,
-  },
-  pricesBarContainer: {
-    backgroundColor: '#A5ABAC',
-    width: 600,
-    height: 40,
-  },
+  }
 });
 
 export default styles;
