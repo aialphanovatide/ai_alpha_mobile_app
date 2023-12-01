014e52b74b49e9a1facd0c170e5d678e44d2274f
FedeMizzau <89542@sistemas.frc.utn.edu.ar>
2023-11-30 21:11:27 -0300

    configuration for CoinGecko API Key

--- services/PriceActionService.js
+++ services/PriceActionService.js
@@ -1,3 +1,4 @@
+import {COINGECKO_API_KEY} from '@env';
 // Funcion para obtener información de todos los coins
 /*
 const coins_to_fetch = [
@@ -42,7 +43,7 @@ const coins_to_fetch = [
 async function getAllCoinsInfo() {
   const coinsInfo = [];
   const response = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ccosmos%2Cpolkadot%2Cquant-network%2Ccardano%2Csolana%2Cavalanche-2%2Cnear%2Cfantom%2Ckaspa%2Cstellar%2Calgorand%2Cripple%2Clido-dao%2Crocket-pool%2Cfrax-share%2Cmatic-network%2Carbitrum%2Coptimism%2Cchainlink%2Capi3%2Cband-protocol%2Cdydx%2Cgmx%2Cvelo%2Cuniswap%2Csushi%2Cpancakeswap-token%2Caave%2Cpendle%2C1inch%2Cocean-protocol%2Cfetch-ai%2Crender-token&per_page=100&page=1&sparkline=false&price_change_percentage=&price_change_percentage=24h%2C7d%2C30d%2C1y&locale=en&precision=2`,
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ccosmos%2Cpolkadot%2Cquant-network%2Ccardano%2Csolana%2Cavalanche-2%2Cnear%2Cfantom%2Ckaspa%2Cstellar%2Calgorand%2Cripple%2Clido-dao%2Crocket-pool%2Cfrax-share%2Cmatic-network%2Carbitrum%2Coptimism%2Cchainlink%2Capi3%2Cband-protocol%2Cdydx%2Cgmx%2Cvelo%2Cuniswap%2Csushi%2Cpancakeswap-token%2Caave%2Cpendle%2C1inch%2Cocean-protocol%2Cfetch-ai%2Crender-token&per_page=100&page=1&sparkline=false&price_change_percentage=&price_change_percentage=24h%2C7d%2C30d%2C1y&locale=en&precision=2&x_cg_demo_api_key=${COINGECKO_API_KEY}`,
   );
   const data = await response.json();
   if (data.length > 0) {
