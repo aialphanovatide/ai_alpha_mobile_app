012966a5f44ebe79196b5db73edbcf61198928fb
FedeMizzau <89542@sistemas.frc.utn.edu.ar>
2024-01-03 20:35:42 -0300

    Solved some errors, device dark mode detection configured

--- App.tsx
+++ App.tsx
@@ -10,20 +10,19 @@ import {
 import Purchases from 'react-native-purchases';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import PaywallScreen from './components/Login/Screens/PaywallScreen';
-import { IOS_API_KEY, ANDROID_API_KEY} from '@env';
-import { ENTITLEMENT_ID } from './src/constants';
+import {IOS_API_KEY, ANDROID_API_KEY} from '@env';
+import {ENTITLEMENT_ID} from './src/constants';
 import Router from './src/navigation/Router';
-import { API_KEY } from './src/constants';
-import { TopMenuContextProvider } from './context/topMenuContext';
-import { UserProvider } from './context/UserContext';
+import {API_KEY} from './src/constants';
+import {TopMenuContextProvider} from './context/topMenuContext';
+import {UserProvider} from './context/UserContext';
 import topTenGainersService from './services/TopTenGainersService';
-import { UserIdProvider } from './context/UserIdContext';
+import {UserIdProvider} from './context/UserIdContext';
 import {CategoriesContextProvider} from './context/categoriesContext';
 import {AppThemeProvider} from './context/themeContext';
 
 const App = () => {
   const colorScheme = Appearance.getColorScheme();
-
   useEffect(() => {
     Purchases.setLogLevel(Purchases.LOG_LEVEL.VERBOSE);
     if (Platform.OS === 'ios') {
@@ -40,34 +39,37 @@ const App = () => {
       //const purchaseMade = await Purchases.purchasePackage(purchasePackage);
       const customerInfo = await Purchases.getCustomerInfo();
 
-        if(typeof customerInfo.entitlements.active[ENTITLEMENT_ID] !== "undefined"){
-          console.log("User is pro");
-        }
-        console.log("Customer info:", customerInfo);
-      } catch (error) {
-        console.log("Error purchasing package:", error);
+      if (
+        typeof customerInfo.entitlements.active[ENTITLEMENT_ID] !== 'undefined'
+      ) {
+        console.log('User is pro');
       }
-
+      console.log('Customer info:', customerInfo);
+    } catch (error) {
+      console.log('Error purchasing package:', error);
+    }
   };
   showUserSubscriptionData();
 
   return (
-    <UserIdProvider>
-      <SafeAreaView
-        style={[
-          styles.container,
-          {backgroundColor: colorScheme === 'dark' ? '#242427' : '#E7EAF1'},
-        ]}>
-        <StatusBar barStyle="dark-content" />
-        <AppThemeProvider>
-          <CategoriesContextProvider>
-            <TopMenuContextProvider>
-              <Navigation />
-            </TopMenuContextProvider>
-          </CategoriesContextProvider>
-        </AppThemeProvider>
-      </SafeAreaView>
-    </UserIdProvider>
+    <UserProvider>
+      <UserIdProvider>
+        <SafeAreaView
+          style={[
+            styles.container,
+            {backgroundColor: colorScheme === 'dark' ? '#242427' : '#E7EAF1'},
+          ]}>
+          <StatusBar barStyle="dark-content" />
+          <AppThemeProvider>
+            <CategoriesContextProvider>
+              <TopMenuContextProvider>
+                <Navigation />
+              </TopMenuContextProvider>
+            </CategoriesContextProvider>
+          </AppThemeProvider>
+        </SafeAreaView>
+      </UserIdProvider>
+    </UserProvider>
   );
 };
 
