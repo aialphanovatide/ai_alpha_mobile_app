2f1f0198d7b3f275ff83376227178bde94529c1e
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-01-23 13:12:33 -0300

    btc dom and eth/btc charts added

--- components/Analysis/BtcDominance/BtcDominance.js
+++ components/Analysis/BtcDominance/BtcDominance.js
@@ -1,16 +1,128 @@
-import {SafeAreaView, Text, View} from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, StyleSheet, Text } from 'react-native';
+import { VictoryChart, VictoryAxis, VictoryCandlestick } from 'victory-native';
+import Loader from '../../Loader/Loader';
+import axios from 'axios';
 import BackButton from '../BackButton/BackButton';
-import useAnalysisStyles from '../AnalysisStyles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const BtcDominance = () => {
-  const styles = useAnalysisStyles();
+const BtcDominanceChart = ({ loading, candlesToShow = 30 }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await axios.get(
+          'https://fapi.binance.com/fapi/v1/klines',
+          {
+            params: {
+              symbol: 'BTCDOMUSDT',
+              interval: '1d',
+              limit: 45,
+            },
+          }
+        );
+        const data = response.data;
+        const ohlcData = data.map((entry) => ({
+          x: new Date(entry[0]),
+          open: parseFloat(entry[1]),
+          high: parseFloat(entry[2]),
+          low: parseFloat(entry[3]),
+          close: parseFloat(entry[4]),
+        }));
+        setChartData(ohlcData);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
+  
+    fetchChartData();
+  }, []); 
+
+  if (loading || chartData.length === 0) {
+    return (
+      <SafeAreaView style={styles.background}>
+        <BackButton />
+        <View style={styles.container}>
+          <Loader />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  const domainY = chartData.reduce(
+    (acc, dataPoint) => [
+      Math.min(acc[0], dataPoint.open, dataPoint.close, dataPoint.high, dataPoint.low),
+      Math.max(acc[1], dataPoint.open, dataPoint.close, dataPoint.high, dataPoint.low),
+    ],
+    [Infinity, -Infinity]
+  );
+
+  const domainX = [chartData[0].x, chartData[chartData.length - 1].x];
+
   return (
     <SafeAreaView style={styles.background}>
       <BackButton />
-      <Text style={styles.messageText}>Coming soon...</Text>
+      <Text style={styles.analysisTitle}>BTC Dominance Chart</Text>
+      <View style={styles.container}>
+        <View style={styles.chart}>
+          <ImageBackground
+            source={require('../../../assets/logo_3.png')}
+            style={styles.backgroundImage}
+          ></ImageBackground>
+
+          <VictoryChart
+            width={400}
+            domain={{ x: domainX, y: domainY }}
+            padding={{ top: 10, bottom: 60, left: 30, right: 60 }}
+            domainPadding={{ x: 5, y: 3 }}
+            scale={{ x: 'time', y: 'linear' }}
+            height={300}
+          >
+            <VictoryAxis dependentAxis orientation="right" />
+            <VictoryAxis />
+
+            <VictoryCandlestick
+              data={chartData}
+              candleRatio={0.5}
+              candleColors={{ positive: '#3ADF00', negative: '#FF477C' }}
+            />
+          </VictoryChart>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
-export default BtcDominance;
+export default BtcDominanceChart;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'top',
+    alignItems: 'top',
+    width: '100%',
+    height: 300,
+  },
+  chart: {
+    marginTop: '15%',
+    width: '100%',
+    height: '90%',
+    margin: '0%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  analysisTitle: {
+    marginTop: '3%',
+    marginLeft: '3%',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  backgroundImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    width: 50,
+    height: 50,
+  },
+});
--- components/Analysis/EthBtcChart/EthBtcChart.js
+++ components/Analysis/EthBtcChart/EthBtcChart.js
@@ -1,16 +1,140 @@
-import {SafeAreaView, Text, View} from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, StyleSheet, Text } from 'react-native';
+import moment from 'moment';
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryCandlestick,
+  VictoryLine,
+  VictoryLabel,
+} from 'victory-native';
+import Loader from '../../Loader/Loader';
+import axios from 'axios';
 import BackButton from '../BackButton/BackButton';
-import useAnalysisStyles from '../AnalysisStyles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const EthBtcChart = () => {
-  const styles = useAnalysisStyles();
+const EthBtcChart = ({ loading, candlesToShow = 30 }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api3.binance.com/api/v3/klines',
+          {
+            params: {
+              symbol: 'ETHBTC',
+              interval: '1d',
+              limit: 45,
+            },
+          }
+        );
+        const data = response.data;
+        const ohlcData = data.map((entry) => ({
+          x: new Date(entry[0]),
+          open: parseFloat(entry[1]),
+          high: parseFloat(entry[2]),
+          low: parseFloat(entry[3]),
+          close: parseFloat(entry[4]),
+        }));
+        setChartData(ohlcData);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
+  
+    fetchChartData();
+  }, []); 
+
+  if (loading || chartData.length === 0) {
+    return (
+      <SafeAreaView style={styles.background}>
+        <BackButton />
+        <Text style={styles.analysisTitle}>ETH/BTC Chart</Text>
+        <View style={styles.container}>
+          <Loader />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  const domainY = chartData.reduce(
+    (acc, dataPoint) => [
+      Math.min(acc[0], dataPoint.low),
+      Math.max(acc[1], dataPoint.high),
+    ],
+    [Infinity, -Infinity]
+  );
+
+  const domainX = [chartData[0].x, chartData[chartData.length - 1].x];
+
   return (
+    <>
     <SafeAreaView style={styles.background}>
       <BackButton />
-      <Text style={styles.messageText}>Coming soon...</Text>
+      <Text style={styles.analysisTitle}>ETH/BTC Chart</Text>
+      <View style={styles.container}>
+        <View style={styles.chart}>
+          <ImageBackground
+            source={require('../../../assets/logo_3.png')}
+            style={styles.backgroundImage}
+          ></ImageBackground>
+
+          <VictoryChart
+            width={400}
+            domain={{ x: domainX, y: domainY }}
+            padding={{ top: 10, bottom: 60, left: 30, right: 60 }}
+            domainPadding={{ x: 5, y: 3 }}
+            scale={{ x: 'time', y: 'linear' }} // Cambiado a escala lineal
+            height={300}
+          >
+            <VictoryAxis dependentAxis orientation="right" />
+            <VictoryAxis />
+
+            <VictoryCandlestick
+              data={chartData}
+              candleRatio={0.5} // Ajusta este valor para controlar el ancho de las velas
+              candleColors={{ positive: '#3ADF00', negative: '#FF477C' }}
+            />
+          </VictoryChart>
+        </View>
+      </View>
     </SafeAreaView>
+    </>
   );
 };
 
 export default EthBtcChart;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'top',
+    alignItems: 'top',
+    width: '100%',
+    height: 500,
+  },
+  chart: {
+    width: '100%',
+    height: '100%',
+    margin: '0%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  analysisTitle:{
+  marginTop: '3%',
+  marginLeft: '3%',
+  fontSize: 16,
+  fontWeight: 'bold', 
+  },
+  backgroundImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    width: 50,
+    height: 50,
+  },
+  background: {
+    flex: 1,
+  },
+});
